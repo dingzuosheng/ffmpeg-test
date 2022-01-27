bc404801f446c16f1aff5d093b9c88354d04793a
dingzuosheng <zuoshengding@gmail.com>
2022-01-27 18:04:39 +0100

    video cut with the tool ffmpeg

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <video
+      id="video"
+      controls
+      src="./assets/video.webm"
+      width="640"
+      height="480"
+    ></video>
+    <video id="videocutRef" width="640" height="480"></video>
+    <button @click="cut()">cut</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+const ffmpeg = createFFmpeg({
+  corePath: "https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js",
+  log: true,
+});
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  methode: {
+    async cut() {
+      await ffmpeg.load();
+      ffmpeg.FS(
+        "writeFile",
+        "watermark.png",
+        await fetchFile("./assets/watermark.png")
+      );
+      ffmpeg.FS("writeFile", "video.webm", fetchFile("./assets/video.webm"));
+      await ffmpeg.run(
+        "-i",
+        "video.webm",
+        "-i",
+        "watermark.png",
+        "-filter_complex",
+        "overlay=x=65:y=355",
+        "result.webm"
+      );
+      const data = ffmpeg.FS("readFile", "result.webm");
+      const video = document.getElementById("videoCutRef");
+      video.src = URL.createObjectURL(
+        new Blob([data.buffer], "result.webm", {
+          type: "video/webm",
+        })
+      );
+    },
+  },
+};
 </script>
 
 <style>
